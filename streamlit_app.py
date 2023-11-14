fef4a96260deb1e28926d9af790cbd0a1ec2d80a
Basel-Najjar <92328645+Basel-Najjar@users.noreply.github.com>
2023-11-14 10:26:57 -0600

    Update streamlit_app.py
    added breakfast menu header and 3 items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 # Snowflake Training - Badge 2: Data Application Builders Workshop
-import streamlit as st
+import streamlit
 
-st.title("Healthy Diner Analysis")
+streamlit.title("Healthy Diner Analysis")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
