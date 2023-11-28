f5fd8b88136bbd7d4d5234b1ea526a6753c892ca
Basel-Najjar <92328645+Basel-Najjar@users.noreply.github.com>
2023-11-28 09:50:24 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Snowflake Training - Badge 2: Data Application Builders Workshop
 import streamlit
-import snowflake.connector
 
 streamlit.title("Healthy Diner Analysis")
 streamlit.header('Breakfast Favorites')
@@ -26,7 +25,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Banana')
 streamlit.write('The user entered ', fruit_choice)
 import requests
 fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_choice}")
@@ -36,4 +35,12 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # display visual
 streamlit.dataframe(fruityvice_normalized)
 
+import snowflake.connector
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
 
